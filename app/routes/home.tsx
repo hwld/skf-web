33a9bdaed3e147d96b90c0bbd29dcfc5740f3dd1
hwld <0701hw@gmail.com>
2025-05-14 10:05:15 +0900

    chore: clean

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,9 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: "skf-web" }, { name: "description", content: "skf-web" }];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div></div>;
 }

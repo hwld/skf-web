ee5aa0b86c959e6493493c9d778af862e89a8560
hwld <0701hw@gmail.com>
2025-05-14 23:53:30 +0900

    feat: ボタンコンポーネントを実装

---
+++ app/components/button.tsx
@@ -0,0 +1,69 @@
+import clsx from "clsx";
+import type { ReactNode } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const button = tv({
+	slots: {
+		base: "flex gap-1 transition duration-100 rounded-sm border items-center px-2",
+		icon: "",
+	},
+	variants: {
+		size: {
+			md: { base: "h-8", icon: "size-5" },
+			sm: { base: "h-7", icon: "size-4" },
+		},
+		color: {
+			primary: {
+				base: "bg-primary-400 text-base-900 border-transparent hover:bg-primary-300",
+				icon: "text-base-900",
+			},
+			secondary: {
+				base: "text-base-100 border-base-500 hover:bg-white/15",
+				icon: "text-base-100",
+			},
+		},
+		disabled: {
+			true: { base: "cursor-not-allowed pointer-events-none opacity-50" },
+		},
+	},
+	defaultVariants: { size: "md", color: "primary" },
+});
+
+type ButtonProps = VariantProps<typeof button> & {
+	leftIconClass?: string;
+	rightIconClass?: string;
+	children: ReactNode;
+};
+
+export function Button({
+	color,
+	size,
+	disabled,
+	leftIconClass,
+	rightIconClass,
+	children,
+	...props
+}: ButtonProps) {
+	const classes = button({
+		color,
+		size,
+		disabled,
+	});
+
+	return (
+		<button
+			{...props}
+			type="button"
+			className={classes.base()}
+			disabled={disabled}
+		>
+			{leftIconClass ? (
+				<span className={clsx(leftIconClass, classes.icon())} />
+			) : null}
+			{children}
+			{rightIconClass ? (
+				<span className={clsx(rightIconClass, classes.icon())} />
+			) : null}
+		</button>
+	);
+}
---
+++ app/components/icon-button.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const button = tv({
+	slots: {
+		base: "rounded-sm grid place-items-center transition duration-100 hover:bg-white/15",
+		icon: "",
+	},
+	variants: {
+		size: {
+			md: { base: "size-8", icon: "size-5" },
+			sm: { base: "size-7", icon: "size-4" },
+		},
+	},
+	defaultVariants: { size: "md" },
+});
+
+type Props = VariantProps<typeof button> & { iconClass: string };
+
+export function IconButton({ size, iconClass, ...props }: Props) {
+	const classes = button({ size });
+
+	return (
+		<button {...props} type="button" className={classes.base()}>
+			<span className={clsx(iconClass, classes.icon())} />
+		</button>
+	);
+}

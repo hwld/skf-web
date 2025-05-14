f5d63fd27e6d1c94bdec04c9830910c0ff74619f
hwld <0701hw@gmail.com>
2025-05-14 11:24:27 +0900

    feat: Problem型の定義と初期データの作成

---
+++ app/data/build-in-problem-set.ts
@@ -0,0 +1,11 @@
+import type { BuildInProblemSet } from "~/models/problem";
+import { allProblems } from "./all-problems";
+
+export const buildInProblemSet: BuildInProblemSet[] = [
+  {
+    id: "1",
+    title: "すべての問題セット",
+    isBuildIn: true,
+    problemIds: allProblems.map((p) => p.id),
+  },
+];

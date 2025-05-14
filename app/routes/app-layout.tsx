8148bd93a10d3cf5ea9f130d188019204e67dc4e
hwld <0701hw@gmail.com>
2025-05-14 17:08:07 +0900

    feat: app-layoutを実装

---
+++ app/routes/app-layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+import { AppHeader } from "../components/app-header";
+
+export default function AppLayout() {
+  return (
+    <div className="grid grid-rows-[auto_1fr] gap-4 h-full">
+      <AppHeader />
+      <Outlet />
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,38 +11,35 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[auto_1fr] gap-4 h-full">
-      <AppHeader />
-      <main className="rounded-lg bg-base-800 border gap-10 border-base-700 grid grid-cols-[1fr_auto] p-6">
-        <div className="flex flex-col gap-4">
-          <PageTitle iconClass="i-tabler-folders" title="問題セット" />
+    <main className="rounded-lg bg-base-800 border gap-10 border-base-700 grid grid-cols-[1fr_auto] p-6">
+      <div className="flex flex-col gap-4">
+        <PageTitle iconClass="i-tabler-folders" title="問題セット" />
 
-          <button className="border border-primary-600 bg-primary-700/15 rounded-lg p-4 w-full gap-2 flex flex-col items-center hover:bg-primary-700/25 transition duration-100">
-            <span className="i-tabler-folder-plus size-8" />
-            <p>Problem Setを作成する</p>
-            <p className="text-base-300 text-xs">
-              ※LocalStorageに保存されるため、キャッシュ削除などで消えてしまう可能性があります。
-            </p>
-            <p></p>
-          </button>
+        <button className="border border-primary-600 bg-primary-700/15 rounded-lg p-4 w-full gap-2 flex flex-col items-center hover:bg-primary-700/25 transition duration-100">
+          <span className="i-tabler-folder-plus size-8" />
+          <p>Problem Setを作成する</p>
+          <p className="text-base-300 text-xs">
+            ※LocalStorageに保存されるため、キャッシュ削除などで消えてしまう可能性があります。
+          </p>
+          <p></p>
+        </button>
 
-          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
-            {buildInProblemSet.map((set) => {
-              return <ProblemSetCard key={set.id} problemSet={set} />;
-            })}
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
+          {buildInProblemSet.map((set) => {
+            return <ProblemSetCard key={set.id} problemSet={set} />;
+          })}
+        </div>
+      </div>
+      <div className="bg-base-950 rounded-lg border border-base-700 p-10 gap-6 hidden xl:flex flex-col 2xl:w-[700px]">
+        <div className="grid place-items-center gap-6 sticky top-[100px] mt-[200px]">
+          <img src="./logo.svg" className="h-10 2xl:h-16" />
+          <div className="text-center text-xs 2xl:text-sm">
+            <p>データサイエンス100本ノック(構造化データ加工編) -SQL-</p>
+            <p> をブラウザで解くことのできるWebアプリです</p>
           </div>
         </div>
-        <div className="bg-base-950 rounded-lg border border-base-700 p-10 gap-6 hidden xl:flex flex-col 2xl:w-[700px]">
-          <div className="grid place-items-center gap-6 sticky top-[100px] mt-[200px]">
-            <img src="./logo.svg" className="h-10 2xl:h-16" />
-            <div className="text-center text-xs 2xl:text-sm">
-              <p>データサイエンス100本ノック(構造化データ加工編) -SQL-</p>
-              <p> をブラウザで解くことのできるWebアプリです</p>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
 

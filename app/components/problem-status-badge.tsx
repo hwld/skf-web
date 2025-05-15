8616cb124894405f570da98dd6381a82d0781f81
hwld <0701hw@gmail.com>
2025-05-16 00:20:51 +0900

    fix: statusを使用している箇所の変更漏れ

--- app/components/problem-status-badge.tsx
+++ app/components/problem-status-badge.tsx
@@ -4,11 +4,14 @@ import type { PlayableProblem } from "./use-playable-problem-set";
 type Props = { status: PlayableProblem["status"] };
 
 export function ProblemStatusBadge({ status }: Props) {
-	const statusClass = {
-		idle: "i-tabler-circle text-base-100",
-		success: "i-tabler-circle-check text-green-400",
-		error: "i-tabler-exclamation-circle text-red-400",
-	}[status];
+	const statusClass = (
+		{
+			idle: "i-tabler-circle text-base-100",
+			right: "i-tabler-circle-check text-green-400",
+			wrong: "i-tabler-exclamation-circle text-red-400",
+			error: "i-tabler-exclamation-circle text-red-400",
+		} satisfies Record<PlayableProblem["status"], string>
+	)[status];
 
 	return <span className={clsx(statusClass, "size-6")} />;
 }
